9b004fefa5450097688452d5b5ffaad6e6f31347
Jesse Skeets <jesseskeets@gmail.com>
2017-09-19 15:25:51 -0700

    Default State Shape

--- StraightUp/frontend/reducers/errors_reducer.js
+++ StraightUp/frontend/reducers/errors_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import SessionErrorsReducer from './session_errors_reducer';
+import UsersErrorsReducer from './users_errors_reducer';
 
 const ErrorsReducer = combineReducers({
-  session: SessionErrorsReducer
+  session: SessionErrorsReducer,
+  users: UsersErrorsReducer
 });
 
 export default ErrorsReducer;
--- StraightUp/frontend/reducers/users_errors_reducer.js
+++ StraightUp/frontend/reducers/users_errors_reducer.js
@@ -1,9 +1,9 @@
-import { RECEIVE_ERRORS} from '../actions/session_actions';
+import { RECEIVE_USER_ERRORS} from '../actions/user_actions';
 
 export default (state = [], action) => {
   Object.freeze(state);
   switch(action.type) {
-    case RECEIVE_ERRORS:
+    case RECEIVE_USER_ERRORS:
       return action.errors;
     default:
       return state;
